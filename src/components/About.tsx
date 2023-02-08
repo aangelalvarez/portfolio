b9e7193b82b56f978648f73893c32f6834af6aa7
Angel Alvarez <angelalvatech@gmail.com>
2023-02-07 23:58:21 -0600

    changed about message

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,13 +9,15 @@ const About: React.FC = () => {
         <div className='About'>
             <h1 className='aboutTitle'>About Me</h1>
             <p className='aboutMe'>
-                I can still remember the first time I got to use a computer, it felt as if I had discovered a new world, so much to explore and do. That was
-                the first time ever I felt that special connection with computers. Eventually I realized that a computer is nothing more than hardware and software, where hardware is the 
-                stuff you can see and touch, and software is the stuff you can see but not touch, where the magic happens.
-                It was not until my senior year of high school that I finally decided to make the move and learn how to create my own software,
-                that is when I discovered my passion for coding.
-                Software is truly incredible because it is the ultimate tool, a tool that can turn
-                into anything you wish it to be, it is the closest thing to magic that exists in this world.             
+            I can still remember the first time I got to use a computer, it felt as if I had discovered a 
+            new world, so much to explore and do. That was the first time I felt that special connection 
+            with computers. Eventually I realized that a computer is both hardware and software, where 
+            hardware is the stuff you can see and touch, and software is the stuff you can see but not 
+            touch, where the magic happens. It was not until my senior year of high school that I finally 
+            decided to make the move and learn how to create my own software, that is when I discovered my 
+            passion for coding. Software is truly incredible because it is the ultimate tool, a tool that 
+            can turn into anything you wish it to be, it is the closest thing to magic that exists in the 
+            world.           
 
             </p>
             <footer className="email">You can contact me at 👉 <footer className="emailColor">angelalvatech@gmail.com</footer></footer>
